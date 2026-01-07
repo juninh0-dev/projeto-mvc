3b57bbee4fc247875dbd2cc3a8ce7f1e3ee479fc
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2026-01-07 10:47:22 -0300

    Adicionando arquivos da aula 2 sobre mvc para enviar um readMe mais organizado.

---
+++ mvc/app/functions/helpers.php
@@ -0,0 +1,5 @@
+<?php
+function dd($dump){
+    var_dump($dump);
+    die();
+}
---
+++ mvc/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '../bootstrap.php';
+
+// https://www.devclass.com.br/curso/show/12/23
+
+$controller = new Controller;
+$controller = $controller->getController();
+
+// $method = new Method;
+// $method = $method->getMethod();
+
+// $parameters = new Parameters;
+// $parameters->getParameters();
+
+
+// $controller->$method($parameters);
